7b39e13c7cd2be7af2e7cb06347ccaa984981240
jsmentch <jsmentch@gmail.com>
2024-08-30 22:04:27 -0400

    wknd

--- code/run_kwyk.sh
+++ code/run_kwyk.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=kwykgpu
+#SBATCH --job-name=kwykstrag
 #SBATCH --output=/om2/user/jsmentch/nat_asd_logs/%x_%j.out 
 #SBATCH --error=/om2/user/jsmentch/nat_asd_logs/%x_%j.err 
 #SBATCH --partition=normal 
-#SBATCH --time=0:30:00
+#SBATCH --time=6:30:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=jsmentch@mit.edu
 #SBATCH --gres=gpu:1
-#SBATCH --array=3-1998
+#SBATCH --array=0-43
 
 #source /om2/user/jsmentch/anaconda/etc/profile.d/conda.sh
 
@@ -18,16 +18,15 @@
 
 #TASK_ID=${roi_id[$SLURM_ARRAY_TASK_ID]}
 
-# while read sub; do sbatch run_kwyk.sh $sub; done < pilots_ru_dm_list.txt
+# OLD # while read sub; do sbatch run_kwyk.sh $sub; done < pilots_ru_dm_list.txt
 
 #sub=$1
 
-#to run:  sbatch --array=3-1998 run_kwyk.sh
+#to run:  sbatch --array=0-43 run_kwyk.sh
 
-# sub=$(sed -n "${SLURM_ARRAY_TASK_ID}p" t1_
-subjects.txt)
-# sub=$(sed -n "${SLURM_ARRAY_TASK_ID}p" kwyk_stragglers.txt)
-sub=$(sed -n "${SLURM_ARRAY_TASK_ID}p" t1_fmriprep_subjects.txt)
+# sub=$(sed -n "${SLURM_ARRAY_TASK_ID}p" t1_subjects.txt)
+sub=$(sed -n "${SLURM_ARRAY_TASK_ID}p" kwyk_stragglers.txt)
+# sub=$(sed -n "${SLURM_ARRAY_TASK_ID}p" t1_fmriprep_subjects.txt)
 
 echo "Running job with SLURM_ARRAY_TASK_ID: ${SLURM_ARRAY_TASK_ID}"
 echo "Running sub: ${sub}"
